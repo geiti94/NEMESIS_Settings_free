888516f28fad67dc20f100381900b0c4e9307268
geiti94 <geiti94@me.com>
2017-02-09 22:00:27 +0100

    Initial release

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/nemesis/settings/free/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nemesis.settings.free.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nemesis.settings.free.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.1";
+}
